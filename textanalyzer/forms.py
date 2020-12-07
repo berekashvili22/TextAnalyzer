6545ef9432b223cbd12737ea86d8800893840763
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-12-08 01:05:57 +0400

    created form for uploading file

--- textanalyzer/forms.py
+++ textanalyzer/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
+from flask_wtf.file import FileField, FileAllowed
 
 class TextForm(FlaskForm):
     text = TextAreaField('', validators=[DataRequired()])
-   
     submit = SubmitField('')
+
+class FileForm(FlaskForm):
+    text_file = FileField('', validators=[FileAllowed(['txt']), DataRequired()])
+    submit2 = SubmitField('')
 
